be9787ec0ddc282dbe4328f0526b490bc71eccf8
sumin <mins19960091@gmail.com>
2024-10-11 18:09:05 +0900

    회원가입 페이징 처리 완료

--- src/main/java/com/boot/practice/controller/JoinController.java
+++ src/main/java/com/boot/practice/controller/JoinController.java
@@ -2,11 +2,17 @@ package com.boot.practice.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class JoinController {
     @GetMapping("/")
-    public String joinPage(){
+    public String login(){
         return "login";
     }
+    @GetMapping("/join/joinPage")
+    public String joinPage(){
+        return "join/joinPage";
+    }
+
 }
